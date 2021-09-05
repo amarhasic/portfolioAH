790656f5f7f4719dd619ae6c0f12dc6e0f53a116
amarhasic <76740130+amarhasic@users.noreply.github.com>
2021-09-05 13:54:03 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,52 +5,51 @@ const contactContent = document.querySelector("#contact-content");
 
 about.addEventListener("click", () => {
   const aboutBox = new WinBox({
-    title: 'About me',
-    background: '#00aa00',
-    width: '400px',
+    title: "About me",
+    background: "#00aa00",
+    width: "400px",
     height: "400px",
     top: 50,
     right: 50,
     bottom: 50,
     left: 50,
     mount: aboutContent,
-    onfocus: function() {
-      this.setBackground('#00aa00')
+    onfocus: function () {
+      this.setBackground("#00aa00");
     },
-    onblur: function() {
-      this.setBackground('#777')
+    onblur: function () {
+      this.setBackground("#777");
     },
-  })
-})
-
+  });
+});
 
 contact.addEventListener("click", () => {
   const contactBox = new WinBox({
-    title: 'Contact me',
-    background: '#00aa00',
-    width: '400px',
+    title: "Contact me",
+    background: "#00aa00",
+    width: "400px",
     height: "400px",
     top: 150,
     right: 50,
     bottom: 50,
     left: 250,
     mount: contactContent,
-    onfocus: function() {
-      this.setBackground('#00aa00')
+    onfocus: function () {
+      this.setBackground("#00aa00");
     },
-    onblur: function() {
-      this.setBackground('#777')
+    onblur: function () {
+      this.setBackground("#777");
     },
   });
 });
 
 work.addEventListener("click", () => {
   window.open("https://lab.hasic.me/", "_blank");
-  
-})
-
+});
 
 resume.addEventListener("click", () => {
-  window.open("https://www.google.com/", "_blank");
-  
-})
+  window.open(
+    "https://docs.google.com/document/d/1luzOgdv-2uQRj9-0XTg4DsvlfIH2QoxT8YQiICWtacs/edit?usp=sharing/",
+    "_blank"
+  );
+});
